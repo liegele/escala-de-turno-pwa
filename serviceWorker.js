7768153070244af1dfc0af05c0aec4b20168a632
liegele <liegele@gmail.com>
2023-01-26 22:30:16 -0300

    Implementando PWA no web app.

---
+++ serviceWorker.js
@@ -0,0 +1,18 @@
+const staticDevEscala = 'dev-escala-site-v1';
+const assets = ['/', '/index.html', '/style.css', '/script.js'];
+
+self.addEventListener('install', (installEvent) => {
+  installEvent.waitUntil(
+    caches.open(staticDevEscala).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (fetchEvent) => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then((res) => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
