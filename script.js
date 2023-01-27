0886ffcf0d1df0255b50e10e9c71efc38f2aaaea
liegele <liegele@gmail.com>
2023-01-26 22:29:34 -0300

    Implementando PWA no webApp.

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-/* // When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-  myFunction();
-};
-
-// Get the navbar
-var gridContainer = document.querySelector('.grid-container-header');
-// Get the offset position of the navbar
-var sticky = gridContainer.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    gridContainer.classList.add('grid-container-header');
-    gridContainer.classList.remove('grid-container');
-  } else {
-    gridContainer.classList.add('grid-container');
-    gridContainer.classList.remove('grid-container-header');
-  }
+//serviveWorker.js registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then((res) => console.log('service worker registered'))
+      .catch((err) => console.log('service worker not registered', err));
+  });
 }
- */
