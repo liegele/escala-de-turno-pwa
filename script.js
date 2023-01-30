006efb28c2b96d90cc466ec7b446b6e7690da71a
liegele <liegele@gmail.com>
2023-01-29 23:39:52 -0300

    Implementando função exibirEscala.

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ document.addEventListener('DOMContentLoaded', exibirEscala);
 
 //Registrando o serviveWorker.js
 
-/* if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
       .register('/serviceWorker.js')
       .then((res) => console.log('service worker registered'))
       .catch((err) => console.log('service worker not registered', err));
   });
-} */
+}
