027f792c555b870c55a7aaececa98266795258fa
liegele <liegele@gmail.com>
2023-01-29 23:32:37 -0300

    Implementando função exibirEscala.

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+const container = document.querySelector('main');
+
 const exibirEscala = function () {
-  console.log('hi');
+  const dataInicial = new Date('01/01/2023');
+  const dataFinal = new Date('12/31/2025');
+  let dataArray = [];
+  let dt = new Date(dataInicial);
+
+  while (dt <= dataFinal) {
+    let event = new Date(dt);
+    let html = `
+    <div class="grid-container">
+      <div class="grid-item">${event.toLocaleDateString('pt-BR')}</div>
+      <div class="grid-item">1</div>
+      <div class="grid-item">2</div>
+      <div class="grid-item">3</div>
+      <div class="grid-item">4</div>
+      <div class="grid-item">5</div>
+    </div>`;
+    // dataArray.push(event.toLocaleDateString('pt-BR'));
+    container.insertAdjacentHTML('beforeend', html);
+    dt.setDate(dt.getDate() + 1);
+  }
+
+  console.log(dataArray);
 };
 
 document.addEventListener('DOMContentLoaded', exibirEscala);
