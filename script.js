63fbb646ca96a0cd2145da50fc5ae7dea41dda85
liegele <liegele@gmail.com>
2023-02-01 13:52:49 -0300

    Desabilitando o service worker.

--- script.js
+++ script.js
@@ -159,11 +159,11 @@ document.querySelector('#hoje').scrollIntoView({ behavior: 'smooth' }); */
 
 //Registrando o serviveWorker.js
 
-if ('serviceWorker' in navigator) {
+/* if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
       .register('/serviceWorker.js')
       .then((res) => console.log('service worker registered'))
       .catch((err) => console.log('service worker not registered', err));
   });
-}
+} */
