b6777edd16e2a5d4674a7784248c4980f5673d99
liegele <liegele@gmail.com>
2023-06-05 13:42:43 -0300

    Atualizando dev

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,30 @@
-const staticDevEscala = 'dev-escala-site-v1';
+const staticDevEscala = 'dev-escala-site-v0.0.3';
 const assets = ['/', '/index.html', '/style.css', '/script.js'];
 
 self.addEventListener('install', (installEvent) => {
+  // self.caches.delete(staticDevEscala);
   installEvent.waitUntil(
     caches.open(staticDevEscala).then((cache) => {
       cache.addAll(assets);
+      //Força a atualização do serviceWorker.js para a versão mais nova.
+      self.skipWaiting();
+      console.log('serviceWorker installed!');
     })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Remove caches antigos
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      return keys.map(async (cache) => {
+        if (cache !== staticDevEscala) {
+          console.log('Service Worker: Removing old cache: ' + cache);
+          return await caches.delete(cache);
+        }
+      });
+    })()
   );
 });
 
@@ -16,3 +35,10 @@ self.addEventListener('fetch', (fetchEvent) => {
     })
   );
 });
+
+/* self.addEventListener('activate', async () => {
+  const tabs = await self.clients.matchAll({ type: 'window' });
+  tabs.forEach((tab) => {
+    tab.navigate(tab.url);
+  });
+}); */
