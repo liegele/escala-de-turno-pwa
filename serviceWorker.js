bef121ae6b86f69b58dabf0e908eb762a962ff5e
liegele <liegele@gmail.com>
2024-01-02 20:12:31 -0300

    Atualizando serviceworker.js para versão v0.0.9

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevEscala = 'dev-escala-site-v0.0.8';
+const staticDevEscala = 'dev-escala-site-v0.0.9';
 const assets = ['/', '/index.html', '/style.css', '/script.js'];
 
 self.addEventListener('install', (installEvent) => {
